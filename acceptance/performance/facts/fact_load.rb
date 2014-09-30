1e8c540adbc6e5e976d2d820fc17369159efa5de
Ken Barber <ken@bob.sh>
2014-09-30 21:31:58 +0100

    (PDB-822) Make the load test more conservative
    Signed-off-by: Ken Barber <ken@bob.sh>

--- acceptance/performance/facts/fact_load.rb
+++ acceptance/performance/facts/fact_load.rb
@@ -1,6 +1,6 @@
 test_name "load series of facts see how long it takes" do
   step "run benchmark" do
-    on(database, "#{LeinCommandPrefix} lein run benchmark -F acceptance/benchmark/facts/ -n 5000 -c acceptance/benchmark/config.ini -N 100 --rand-perc 10")
+    on(database, "#{LeinCommandPrefix} lein run benchmark -F acceptance/benchmark/facts/ -n 5000 -c acceptance/benchmark/config.ini -N 5 --rand-perc 10")
   end
 
   step "wait max 6 minutes for completion" do
